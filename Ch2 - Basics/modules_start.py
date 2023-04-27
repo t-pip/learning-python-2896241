c8bb2ff9091c186dd1ece57b275e48e9ead68823
t-pip <trev.junk@gmail.com>
2023-04-27 18:41:14 +0100

    Completed

--- Ch2 - Basics/modules_start.py
+++ Ch2 - Basics/modules_start.py
@@ -3,10 +3,11 @@
 
 
 # TODO: import the math module, which contains features for working with mathematics
-
+import math
+print("The square root of 16 is ", math.sqrt(16))
 
 # TODO: the math module contains lots of pre-built functions
-
+print("pi is ", math.pi)
 
 # TODO: in addition to functions, some modules contain useful constants 
 
